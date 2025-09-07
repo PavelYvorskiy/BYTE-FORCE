206805fb7a23be3139c10ae10cfd2e360004f788
Pavel Yvorskiy <pavelyvorski@icloud.com>
2025-09-07 21:47:11 +0300

    main

---
+++ app.js
@@ -0,0 +1,5 @@
+import './assets/script/header.js';
+import './assets/script/vanilla-tilt.js';
+import './assets/script/card-anim.js';
+import './assets/script/aos.js';
+import './assets/script/scrollAnimation.js';
---
+++ assets/script/card-anim.js
@@ -0,0 +1,13 @@
+VanillaTilt.init(document.querySelectorAll(".cardGlass"), {
+  max: 0,
+  speed: 3000,
+  glare: true,
+  "max-glare": 1,
+});
+
+VanillaTilt.init(document.querySelectorAll(".serviseCard"), {
+  max: 5,
+  speed: 3000,
+  glare: true,
+  "max-glare": 3,
+});
